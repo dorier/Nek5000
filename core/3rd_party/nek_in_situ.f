2afa9a59547092c155ea4375d1a369c63ffbfcd7
Matthieu Dorier <mdorier@anl.gov>
2017-06-01 09:09:57 -0500

    started adding Damaris support

--- core/3rd_party/nek_in_situ.f
+++ core/3rd_party/nek_in_situ.f
@@ -3,17 +3,25 @@ c-----------------------------------------------------------------------
 #ifdef VISIT
       call visit_init()
 #endif
+! Damaris is initialized earlier in the code, as it requires
+! to change MPI_COMM_WORLD
       end
 c-----------------------------------------------------------------------
       subroutine in_situ_check()
 #ifdef VISIT
       call visit_check()
 #endif
+#ifdef ENABLE_DAMARIS
+      call damaris_check()
+#endif
       end
 c-----------------------------------------------------------------------
       subroutine in_situ_end()
 #ifdef VISIT
       call visit_end()
+#endif
+#ifdef ENABLE_DAMARIS
+      call damaris_end()
 #endif
       end
 c-----------------------------------------------------------------------
